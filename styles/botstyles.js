37af0b86945651615fb264276dfe5898ef538674
Solale90 <solaleh.rm@gmail.com>
2019-10-13 21:30:55 -0700

    Add missing style.

---
+++ styles/botstyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+    Bot:{
+      backgroundColor:"#feffe6",
+      width:"100%",
+      padding:20,
+      justifyContent:"center",
+      alignItems:"center",
+      flex: 1,
+      flexDirection: 'row'
+    },
+    BotText:{
+    }
+});
+
+export default styles;
