5f4488a14bd1535d558a57433270a4678e29ef57
Solale90 <solaleh.rm@gmail.com>
2019-10-13 21:02:13 -0700

    Add Top component

---
+++ styles/mainstyles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+    main:{
+       position: "relative",
+       width: "100%",
+       height: "100%",
+       justifyContent:"center",
+       alignItems: "center" 
+    }
+});
+
+export default styles;
---
+++ styles/topstyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+    Top:{
+      width:"100%",
+      padding:50,
+      justifyContent:"center",
+      alignItems:"center",
+      flex: 1
+    },
+    TopText:{
+        color:"#FFF",
+        fontWeight:"bold"
+    }
+});
+
+export default styles;
