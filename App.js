826196700b3abc562a6d38dad4c0d83544f2b6fc
solale90 <solaleh.rm@gmail.com>
2019-10-13 20:37:13 -0700

    Add react native project.

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './styles/appstyles';
+import Main from './Comps/Main';
+
+function App(){
+  return (
+    <View style={styles.app}> 
+      <Main/>
+    </View>
+  );
+}
+
+export default App;
